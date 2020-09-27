e977107ac871985f474e9632d35f9dcec3f0fc6c
kengy <kengyry.ken@yandex.ru>
2020-09-27 13:53:35 +0500

    final commit

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    private final Duck5 duck;
+
+    // Вариант первый - связывание через конструктор
+    @Autowired
+    public Rabbit4(Duck5 duck) { this.duck = duck; }
+
     @Override
-    public String toString() {
-        return ", в зайце утка " + "";
-    }
+    public String toString() { return ", в зайце утка " + duck.toString(); }
 }
